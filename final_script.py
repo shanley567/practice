2e31d7058acc7114847f3305465b9e3bcf92a3df
shanley567 <js10587@hotmail.com>
2024-03-27 11:43:50 +0000

    changed spacing

--- final_script.py
+++ final_script.py
@@ -8,8 +8,6 @@ from merge_pdfs import merge_pdfs
 from add_page_numbers_to_pdf import add_page_numbers
 
 
-
-
 folder_path = r"C:\Users\js105\Documents\Work\Dad's Work"   
 
 word_to_pdf(folder_path)
